19be0ca70df7984356ca8b384ef8d16f3a08be3d
imbesky <123881839+imbesky@users.noreply.github.com>
2023-02-01 19:05:43 +0900

    Update VanilaJS-4-1.js

--- VanilaJS-4-1.js
+++ VanilaJS-4-1.js
@@ -1,16 +1,18 @@
-const clock = document.querySelector("h2#clock");
+const clock = document.querySelector("h2.js-clock");
 
-function time(){
-    const ADay = 1000*60*60*24;
-    const ChristmasDate = new Date("December 25 2023 00:00:00 GMT+0900");
-    const Nowdate = new Date();
-    const RemainingDate = 
-    new Date(ChristmasDate.getTime() - Nowdate.getTime());
-    const RemainingD = Math.trunc(RemainingDate/ADay);
-    const RemainingH = String(RemainingDate.getHours()).padStart(2, "0");
-    const RemainingM = String(RemainingDate.getMinutes()).padStart(2, "0");
-    const RemainingS = String(RemainingDate.getSeconds()).padStart(2, "0");
-    clock.innerText = 
-    `${RemainingD}d ${RemainingH}h ${RemainingM}m ${RemainingS}s`;
+function time() {
+  const ADay = 1000 * 60 * 60 * 24;
+  const ChristmasDate = new Date("2023, 12, 25");
+  const Nowdate = new Date();
+  const RemainingDate = new Date(
+    ChristmasDate.getTime() -
+      Nowdate.getTime() +
+      +Nowdate.getTimezoneOffset() * 60 * 1000
+  );
+  const RemainingD = Math.trunc(RemainingDate / ADay);
+  const RemainingH = String(RemainingDate.getHours()).padStart(2, "0");
+  const RemainingM = String(RemainingDate.getMinutes()).padStart(2, "0");
+  const RemainingS = String(RemainingDate.getSeconds()).padStart(2, "0");
+  clock.innerText = `${RemainingD}d ${RemainingH}h ${RemainingM}m ${RemainingS}s`;
 }
 setInterval(time, 1000);
