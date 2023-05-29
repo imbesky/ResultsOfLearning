a5d01ab79daf5834d4e87292cdec50858f700713
imbesky <123881839+imbesky@users.noreply.github.com>
2023-05-29 23:11:26 +0900

    Add files via upload

---
+++ KeyElements.java
@@ -0,0 +1,14 @@
+package elements;
+
+public class KeyElements {
+	public static void main(String[] args) {
+		String variable, dataType;
+		variable = "변수";
+		dataType = "자료형";
+		System.out.println(variable + "는 " + dataType + "을 가집니다.");
+		
+		float f;
+		f = 3.14f; //실수형 자료는 기본적으로 double로 인식됨
+		
+	}
+}
