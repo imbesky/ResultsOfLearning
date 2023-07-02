a843dd5be86231eebd68d957bd6a435b4d971aea
imbesky <123881839+imbesky@users.noreply.github.com>
2023-07-02 18:02:36 +0900

    Update Universe.java

--- PrivateConstructor/Universe.java
+++ PrivateConstructor/Universe.java
@@ -3,6 +3,7 @@ public class Universe{
     
   }
   public void sun(){
+    //static 키워드가 없으면 사용 불가
     System.out.println("태양은 항성입니다");
     //System도 constructor가 private한 class라서 이런 형태로 사용
     //메서드들은 전부 static함
