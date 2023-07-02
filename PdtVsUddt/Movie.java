c75e780f65b7378ac3604d7c152767aca85942ad
imbesky <123881839+imbesky@users.noreply.github.com>
2023-07-02 09:14:57 +0900

    Create Movie.java

---
+++ PdtVsUddt/Movie.java
@@ -0,0 +1,10 @@
+public class Movie {
+    public String title;
+    public int length;
+    public String director;
+    public int cost;
+  //디폴트 생성자 메서드가 원래는 생략되어 있는 상태
+  public Movie(){
+    //객체를 생성하는 작업을 함 - 기계어 코드
+  }
+  }
