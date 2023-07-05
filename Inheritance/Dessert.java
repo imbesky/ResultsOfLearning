5ef9a36b10523df6ade8850baebca5613597e251
imbesky <123881839+imbesky@users.noreply.github.com>
2023-07-05 11:44:04 +0900

    Create Dessert.java

---
+++ Inheritance/Dessert.java
@@ -0,0 +1,9 @@
+public class Dessert extends Object{
+  //포괄적, 추상적인 메서드
+  public void ingredient(){
+    System.out.println("재료");
+  }
+  public void temperature(){
+    System.out.println("온도");
+  }
+}
