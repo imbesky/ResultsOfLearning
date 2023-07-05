97d6afab25e5df802bd9b65852b8a376f5cdf7f8
imbesky <123881839+imbesky@users.noreply.github.com>
2023-07-05 11:43:39 +0900

    Create Financier.java

---
+++ Inheritance/Financier.java
@@ -0,0 +1,9 @@
+public class Financier extends Dessert{
+  //overriding
+  public void temperature(){
+    System.out.println(190);
+  }
+  public void texture(){
+    System.out.println("표면이 바삭합니다.");
+  }
+}
