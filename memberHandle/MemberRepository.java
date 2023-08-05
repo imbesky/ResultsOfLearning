6eb546d092f3836ffc3b1674ba457014fac38f20
imbesky <123881839+imbesky@users.noreply.github.com>
2023-08-05 15:44:22 +0900

    Create MemberRepository.java

---
+++ memberHandle/MemberRepository.java
@@ -0,0 +1,11 @@
+package sky.demo.repository;
+import sky.demo.domain.Member;
+import java.util.Optional;
+import java.util.List;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
